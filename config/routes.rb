ffb113d0d72444c95742f50d4ca8972efb1968ee
Emilea Teo <emileateo@gmail.com>
2021-05-11 15:35:58 +0800

    user can approve playdates now

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   resources :interests, only: [:index]
 
+  patch '/interests/:id/approve', to: 'interests#approve', as: :approve
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
