e711d957911a43853b43ca772803e7c81a58542f
Thomas Goldsbury <goldsbury.thomas@hotmail.com>
2021-05-12 19:08:47 +0800

    Added the update feature

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -22,10 +22,23 @@ class PetsController < ApplicationController
     end
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def pet_params
-    params.require(:pet).permit(:name, :description, :temperament, :breed, :photo )
+    params.require(:pet).permit(:name, :description, :temperament, :breed, :photo)
   end
 
   def set_pet
