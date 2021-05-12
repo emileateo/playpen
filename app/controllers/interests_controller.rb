65b8f62fb47299f9b2d6a9186b6264066769c0f0
Emilea Teo <emileateo@gmail.com>
2021-05-12 09:59:34 +0800

    logic for interests changed

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,14 +1,25 @@
 class InterestsController < ApplicationController
   def index
     @interests = Interest.all
-    @current_user_interests = Interest.where(user: current_user)
+    @pets = Pet.where(user: current_user)
+    # @current_user_interests = Interest.where(pet: current_user)
+    # I want to find all the interests that User A (Current User) received
+    # HELPPPPP
+    @current_user_interests = []
+    @interests.all.each do |interest|
+      if @pets.include? interest.pet
+        @current_user_interests << interest
+      end
+    end
   end
 
   def create
     @interest = Interest.new(interest_params)
     @pet = Pet.find(params[:pet_id])
     @interest.pet = @pet
+    # This pet is the pet who receives the request
     @interest.user = current_user
+    # This user is the user who sends the request
 
     @interest.status = false
 
