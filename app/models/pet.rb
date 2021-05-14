d3ccffea61183984c936da08d9be9bc37ce95c7f
Emilea Teo <emileateo@gmail.com>
2021-05-14 12:37:05 +0800

    added search function

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,11 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_many :interests, dependent: :destroy
+
+  include PgSearch::Model
+  pg_search_scope :search_by_breed,
+                  against: %i[breed],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
