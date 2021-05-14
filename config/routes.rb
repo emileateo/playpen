e4fe13a52e68d1dabad0df3f7467b1e1dae99d60
Thomas Goldsbury <goldsbury.thomas@hotmail.com>
2021-05-14 15:40:08 +0800

    User profile now shows all the dogs linked to a specific user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :users, only: [:index, :show]
   resources :pets, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :interests, only: [:create]
   end
