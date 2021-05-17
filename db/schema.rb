f6d7fdd08f535949dd65400e32a57ec02e730afc
AidenAng <aidenangdj@gmail.com>
2021-05-17 15:58:03 +0800

    availability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_074503) do
+ActiveRecord::Schema.define(version: 2021_05_17_075520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,13 +47,6 @@ ActiveRecord::Schema.define(version: 2021_05_17_074503) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["pet_id"], name: "index_interests_on_pet_id"
     t.index ["user_id"], name: "index_interests_on_user_id"
-  end
-
-  create_table "meetings", force: :cascade do |t|
-    t.string "name"
-    t.datetime "start_time"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "pets", force: :cascade do |t|
